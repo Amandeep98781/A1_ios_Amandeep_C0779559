087a23733e4538e28db11b32e2489a9a082b0c0a
Amandeep98781 <77733148+Amandeep98781@users.noreply.github.com>
2021-01-25 13:09:21 -0500

    Initial Commit

---
+++ A1_ios_Amandeep_C0779559/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  A1_ios_Amandeep_C0779559
+//
+//  Created by Amandeep Kaur on 25/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
